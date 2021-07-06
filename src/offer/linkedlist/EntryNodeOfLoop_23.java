11df8ec0c72f0f3f0f6fa8186a2f331f2359de1a
cxylk <cxylikui@163.com>
2021-07-06 11:03:50 +0800

    :construction: 链表中倒数第K个节点和环的入口节点

---
+++ src/offer/linkedlist/EntryNodeOfLoop_23.java
@@ -0,0 +1,88 @@
+package offer.linkedlist;
+
+/**
+ * @Classname EntryNodeOfLoop_23
+ * @Description 链表中环的入口节点   三个步骤：
+ *              1、判断链表是否有环，使用快慢指针
+ *              2、判断链表中环的节点个数
+ *              3、找到环的入口节点
+ *              两个测试用例：{1,2,3},{4,5,6,7}和{},{1,2,3,4,5}
+ * @Author likui
+ * @Date 2021/7/6 10:05
+ **/
+public class EntryNodeOfLoop_23 {
+    /**
+     * 第一种解法：使用3个步骤
+     * @param pHead
+     * @return
+     */
+    public ListNode entryNodeOfLoop(ListNode pHead){
+        if(pHead==null||pHead.next==null){
+            return null;
+        }
+        //两个快慢指针，刚开始时两者间隔1个节点，后移移动间隔2个节点
+        ListNode slow=pHead;
+        ListNode fast=pHead;
+        //是否存在环的标记
+        boolean flag=false;
+        //1、判断链表是否存在环
+        while (fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            //如果两者相遇，说明存在环
+            if(slow==fast){
+                flag=true;
+                break;
+            }
+        }
+        //如果不存在环直接返回null
+        if(!flag){
+            return null;
+        }else {
+            //2、得到环中节点个数,用n表示
+            int n=1;
+            fast=fast.next;
+            while (slow!=fast){
+                fast=fast.next;
+                n++;
+            }
+            //找到环中入口节点
+            //让slow,fast重新从头节点开始
+            slow=pHead;
+            fast=pHead;
+            for (int i = 0; i < n; i++) {
+                fast=fast.next;
+            }
+            //两者相遇处即为环的入口节点
+            while (slow!=fast){
+                slow=slow.next;
+                fast=fast.next;
+            }
+            return slow;
+        }
+    }
+
+    /**
+     * 第二种解法，不用求出环中节点个数
+     * @param pHead
+     * @return
+     */
+    public ListNode entryNodeOfLoop2(ListNode pHead){
+        if(pHead==null||pHead.next==null){
+            return null;
+        }
+        ListNode slow=pHead,fast=pHead;
+        //初始化，两个节点间隔1个节点
+        do{
+            slow=slow.next;
+            fast=fast.next.next;
+        }while (slow!=fast);
+        //让fast指针重新从头开始走(slow也一样，因为两者在上一步是重合的）
+        fast=pHead;
+        while (slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+    }
+}
---
+++ src/offer/linkedlist/KthNodeFromEnd_22.java
@@ -0,0 +1,88 @@
+package offer.linkedlist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Classname KthNodeFromEnd_22
+ * @Description 链表中倒数第K个节点
+ * @Author likui
+ * @Date 2021/7/5 20:37
+ **/
+public class KthNodeFromEnd_22 {
+    public ListNode findKthToTail(ListNode pHead, int k) {
+        /**
+         * 需要两次遍历链表，时间复杂度为O(N)
+         */
+        if (pHead == null || k == 0)
+            return null;
+        //第一次遍历链表，得到链表的长度
+        int count = 0;
+        ListNode node = pHead;
+        while (node.next != null) {
+            node = node.next;
+            count++;
+        }
+        //如果K大于链表长度，返回长度为0的链表
+        if (k > count) {
+            return null;
+        }
+        //第二次遍历链表，找到第K个节点的位置
+        for (int i = 0; i <= count; i++) {
+            //倒数第K个节点的位置=链表长度-K+1，加1是因为下标从0开始
+            if (i == count - k + 1)
+                return pHead;
+            pHead = pHead.next;
+        }
+        return null;
+    }
+
+    public ListNode findKthToTail2(ListNode pHead, int k) {
+        if(pHead==null||k==0)
+            return null;
+        //声明两个指针
+        ListNode p1=pHead;
+        //让p1和p2保持k-1的距离，这样当p1指向尾结点时，p2刚好是倒数第k个节点
+//        for (int i = 0; i < k - 1; i++) {
+//            //p1指针开始向前走K-1步
+//            //防止K>链表的情况
+//            if(p1.next!=null) {
+//                p1 = p1.next;
+//            }else {
+//                return null;
+//            }
+//
+//        }
+//        //p2指向首节点
+//        ListNode p2=pHead;
+//        while (p1.next!=null){
+//            //从第K步开始，让p1，p2一起向前走
+//            p1=p1.next;
+//            p2=p2.next;
+//        }
+//        return p2;
+
+
+
+        while (p1 != null && k-- > 0)
+            p1 = p1.next;
+        if (k > 0)
+            return null;
+        //p2指向首节点
+        ListNode p2=pHead;
+        while (p1!=null){
+            //从第K步开始，让p1，p2一起向前走
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return p2;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(1);
+        System.out.println(set);
+    }
+
+}
