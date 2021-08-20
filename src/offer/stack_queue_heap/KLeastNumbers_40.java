ae207702c27892aa36c4388f155cea63ba8538b1
cxylk <cxylikui@163.com>
2021-08-20 10:01:10 +0800

    :bug: 修正

--- src/offer/stack_queue_heap/KLeastNumbers_40.java
+++ src/offer/stack_queue_heap/KLeastNumbers_40.java
@@ -15,6 +15,8 @@ public class KLeastNumbers_40 {
      * 第一种方式是使用大顶堆实现（小顶堆的话需要将所有元素都插入堆，然后取最小的k个元素，大顶堆只需要维护前k个最小值）
      * 时间复杂度为：O(NlogK)，最坏情况下，k=nums.length，那么会将n个数都插入堆
      * 空间复杂度：O(K)
+     *
+     * 如果要求第k个最大的数呢？使用小顶堆，最后返回堆顶元素即可
      * @param nums
      * @param k
      * @return
@@ -65,10 +67,10 @@ public class KLeastNumbers_40 {
             //nums[pos]就是数组第pos大元素
             if(pos==k)
                 break;
+            //最小的k个数还要往前找
             if(pos>k)
                 h=pos-1;
-            //pos左边的元素已经是有序的了
-            //这时要从pos+1开始排序
+            //最小的k个数还要往后找
             else
                 l=pos+1;
         }
@@ -76,17 +78,23 @@ public class KLeastNumbers_40 {
 
     private int partition(int[] nums, int l, int h) {
         int i=l;
+        //j=h+1是因为while循环中会先执行--操作
         int j=h+1;
-        //取第一个为基准元素
+        //取第一个为基准元素pivot
         int val=nums[l];
         while (true){
+            //保证nums[l..i]都小于pivot
             while (i != h && nums[++i] < val) ;
+            //保证nums[j..h]都大于pivot
             while (j != l && nums[--j] > val) ;
             if (i >= j)
                 break;
+            //如果走到这里一定有nums[i]>pivot&&nums[j]<pivot
+            //所以需要交换nums[i]和nums[j]
+            //保证nums[l..i]<pivot<nums[j..h]
             nums[i]=nums[j]^(nums[j]=nums[i])^nums[i];
         }
-        //循环结束将最小元素放在前面
+        //循环结束将基准元素pivot交换到正确的位置
         nums[l]=nums[j]^(nums[j]=nums[l])^nums[l];
         return j;
     }
